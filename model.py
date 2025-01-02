4964ebbaab54f8c1f59ed6bcae41d136dd662851
NPOS <npostring@gmail.com>
2025-01-02 17:07:52 +0900

    Initial commit

---
+++ model.py
@@ -0,0 +1,202 @@
+import math
+
+import cv2
+import numpy as np
+
+
+def sampling_clustering(img, pixel_size=8):
+    '''
+    Purely image reduction and enlargement method.
+    '''
+    # sampling(shrink)
+    org_shape = img.shape
+    ratio = 1 / pixel_size
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    img = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+
+    # sampling(expand)
+    img = cv2.resize(img, (org_shape[1], org_shape[0]), interpolation=cv2.INTER_NEAREST)
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+
+    return img
+
+
+def k_means_clustering(img, k=16, pixel_size=8):
+    # sampling(shrink)
+    org_shape = img.shape
+    ratio = 1 / pixel_size
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    img = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+
+    # k-means
+    out_img = img.reshape(-1, 3).astype(np.float32)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    _, labels, centers = cv2.kmeans(out_img, k, None, criteria, 10, cv2.KMEANS_PP_CENTERS)
+    centers = centers.astype(np.uint8)
+    out_img = centers[labels.flatten()].reshape(img.shape)
+
+    # sampling(expand)
+    img = cv2.resize(out_img, (org_shape[1], org_shape[0]), interpolation=cv2.INTER_NEAREST)
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+
+    return img
+
+    
+def mda_clustering(img, color_num=16, color_thresh=10, pixel_size=8):
+    '''
+    Maximum Distance Algorithm
+    https://www.jstage.jst.go.jp/article/itej/66/11/66_J399/_pdf
+    '''
+
+    # sampling(shrink)
+    org_shape = img.shape
+    ratio = 1 / pixel_size
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    img = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+ 
+    # select representative color
+    target = np.array((127, 127, 127), dtype=np.int32)
+    max_dist = 0
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            pixel = img[y, x].astype(np.int32)
+            dist = math.sqrt(((pixel[0]-target[0])*(100/255))**2 + ((pixel[1]-target[1])*(120/255))**2 + ((pixel[2]-target[2])*(120/255))**2)
+            if dist > max_dist:
+                max_dist = dist
+                max_y = y
+                max_x = x
+    target = img[max_y, max_x].astype(np.int32)
+
+    # setting initial cluster
+    clusters = np.zeros((img.shape[0], img.shape[1]), dtype=np.int32)
+    min_dists = np.zeros((img.shape[0], img.shape[1]), dtype=np.float32)
+    for y in range(min_dists.shape[0]):
+        for x in range(min_dists.shape[1]):
+            min_dists[y, x] = 1000000
+
+    # update cluster
+    cur_k = 0
+    while True:
+        # caluculate distance
+        max_dist = 0
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+                pixel = img[y, x].astype(np.int32)
+                dist = math.sqrt(((pixel[0]-target[0])*(100/255))**2 + ((pixel[1]-target[1])*(120/255))**2 + ((pixel[2]-target[2])*(120/255))**2)
+                if dist < min_dists[y, x]:
+                    min_dists[y, x] = dist
+                    clusters[y, x] = cur_k
+                    if dist > max_dist:
+                        max_dist = dist
+                        max_y = y
+                        max_x = x
+
+        # if the distance is less than the threshold, the process is terminated
+        if max_dist > color_thresh and cur_k < color_num:
+            target = img[max_y, max_x].astype(np.int32)
+            cur_k += 1
+        else:
+            break
+
+    # calculate the centroid of each cluster and replace the color of the pixel with the centroid color
+    color_palette = []
+    for k_num in range(cur_k):
+        y, x = np.where(clusters == k_num)
+        color = np.zeros((3), dtype=np.int64)
+        for ii in range(len(y)):
+            color[0] += img[y[ii], x[ii]][0]
+            color[1] += img[y[ii], x[ii]][1]
+            color[2] += img[y[ii], x[ii]][2]
+        color = (color / len(y)).astype(np.uint8)
+        img[y, x] = color
+        color_palette.append(color)
+
+    # sampling(expand)
+    img = cv2.resize(img, (org_shape[1], org_shape[0]), interpolation=cv2.INTER_NEAREST)
+    img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+
+    return img
+
+def ndcd_clustering(img, h):
+    '''
+    Generating Pixel Art by Noniterative Dominant Color Decision Algorithm
+    https://www.jstage.jst.go.jp/article/itej/74/3/74_597/_pdf
+    '''
+    org_shape = img.shape
+
+    M = img.shape[0] // h
+    N = img.shape[1] // h
+    out_img = np.zeros((M, N, 3), dtype=np.uint8)
+
+    # Divide the image into M x N blocks
+    for i in range(M):
+        for j in range(N):
+            dist1 = cv2.norm(img[i*h][j*h], img[(i+1)*h-1][(j+1)*h-1], cv2.NORM_L2)
+            dist2 = cv2.norm(img[i*h][(j+1)*h-1], img[(i+1)*h-1][j*h], cv2.NORM_L2)
+            if dist1 > dist2:
+                u = img[i*h][j*h]
+                v = img[(i+1)*h-1][(j+1)*h-1]
+            else:
+                u = img[i*h][(j+1)*h-1]
+                v = img[(i+1)*h-1][j*h]
+
+            # clustering u and v 
+            u_pixels = []
+            v_pixels = []
+            for iu in range(h):
+                for jv in range(h):
+                    dist_iu = cv2.norm(u, img[i*h+iu][j*h+jv], cv2.NORM_L2)
+                    dist_jv = cv2.norm(v, img[i*h+iu][j*h+jv], cv2.NORM_L2)
+                    if dist_iu > dist_jv:
+                        u_pixels.append(img[i*h+iu][j*h+jv])
+                    else:
+                        v_pixels.append(img[i*h+iu][j*h+jv])
+
+                    # calculate the average color of the many pixels in the block(u or v) 
+                    if len(u_pixels) > len(v_pixels):
+                        out_img[i, j] = np.mean(u_pixels, axis=0)
+                    else:
+                        out_img[i, j] = np.mean(v_pixels, axis=0)
+
+    # sampling(expand)
+    img = cv2.resize(out_img, (org_shape[1], org_shape[0]), interpolation=cv2.INTER_NEAREST)
+
+    return img
+
+
+def convert(img, pixel_size=8, color_num=4, color_thresh=10, mode='kmeans', is_noise_reduction=True):
+    # save alpha channel
+    before_img = img.copy()
+    if before_img.shape[2] == 4:
+        img = img[:, :, :3]
+
+    # resize to 1500px or less when the image size is huge
+    ratio = None
+    max_size = 1500
+    if img.shape[0] > max_size or img.shape[1] > max_size:
+        ratio = max_size / max(img.shape[0], img.shape[1])
+        img = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+
+    # noise reduction
+    if is_noise_reduction:
+        img = cv2.bilateralFilter(img, 5, 50, 20)
+
+    # clustering
+    if mode == 'sampling':
+        img = sampling_clustering(img, pixel_size)
+    if mode == 'k-means':
+        img = k_means_clustering(img, color_num, pixel_size)
+    elif mode == 'mda':
+        img = mda_clustering(img, color_num=color_num, color_thresh=color_thresh, pixel_size=pixel_size)
+    elif mode == 'ndcd':
+        img = ndcd_clustering(img, pixel_size)
+
+    if ratio is not None:
+        img = cv2.resize(img, (before_img.shape[1], before_img.shape[0]), interpolation=cv2.INTER_NEAREST)
+   
+    # restore alpha channel
+    if before_img.shape[2] == 4:
+        before_img[:,:,:3] = img
+        img = before_img
+
+    return img
---
+++ view.py
@@ -0,0 +1,143 @@
+import cv2
+import numpy as np
+import streamlit as st
+
+import model
+
+
+# wide mode
+st.set_page_config(
+    page_title='npixelate',
+    page_icon='💠',
+    layout="centered",
+    initial_sidebar_state="expanded"
+    )
+
+def initialize():
+    # initialize session state
+    if 'mode' not in st.session_state:
+        st.session_state.mode = 'k-means'
+    if 'pixel_size' not in st.session_state:
+        st.session_state.pixel_size = 4
+    if 'color_num' not in st.session_state:
+        st.session_state.color_num = 8
+    if 'color_thresh' not in st.session_state:
+        st.session_state.color_thresh = 10
+    if 'is_noise_reduction' not in st.session_state:
+        st.session_state.is_noise_reduction = False
+
+    # hidden topbar and adjust width
+    st.markdown("""
+        <style>
+        header.stAppHeader {
+            background-color: transparent;
+        }
+        section.stMain .block-container {
+            padding-top: 0rem;
+            z-index: 1;
+            max-width: 800px;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # apply css
+    with open('style.css') as css:
+        pass
+        st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+
+def main():
+    # sidebar
+    with st.sidebar:
+        st.header('How to use')
+        st.write('１.「Browse files」をクリックし画像をアップロード')
+        st.write('２. パラメーターを設定')
+        st.write('３.「ドット化」ボタンをクリックしドット化')
+        st.write('４.「ダウンロード」ボタンをクリックしダウンロード')
+        st.header('Author')
+        st.write(':red[npostring]')
+        st.link_button('Website', 'https://www.dozingwhale.net')
+        st.link_button('GitHub', 'https://github.com/npostring')
+        st.link_button('Twitter', 'https://x.com/npostring')
+        st.write('Ⓒ 2025 Dozing Whale')
+
+    # title
+    st.markdown("<h1 style='text-align: center; padding-top: 0rem'>NPixelate (ドット絵変換ツール)</h1>", unsafe_allow_html=True)
+
+    # upload image
+    uploaded_img = st.file_uploader('画像をアップロード(5MBまで)', type=['jpg', 'jpeg', 'png'])
+    if uploaded_img:
+        st.session_state.uploaded_img = uploaded_img
+    else:
+        if 'uploaded_img' in st.session_state:
+            del st.session_state.uploaded_img
+        if 'pixelated_img' in st.session_state:
+            del st.session_state.pixelated_img
+
+    # set parameter
+    with st.container(border=True, height=350):
+        st.write('パラメーター設定')
+        col1, col2 = st.columns(2)
+        with col1:
+            st.selectbox('変換モード', options=['k-means', 'mda', 'sampling', 'ndcd'], key='mode', index=0,
+                         help='・k-means：イラストにおすすめ。\n\n・mda：イラストにおすすめ(遅いけどくっきり)。\n\n・sampling：写真におすすめ。\n\n・ndcd：写真におすすめ(遅いけど自然)。')
+            st.checkbox('ノイズ除去', key='is_noise_reduction', help='ドット化前にわずかにぼかすことでノイズを除去する。')
+        with col2:
+            st.slider('ドットサイズ', 2, 32, int(st.session_state.pixel_size), step=1, key='pixel_size')
+            if st.session_state.mode == 'sampling':
+                pass
+            if st.session_state.mode == 'k-means':
+                st.slider('色数', 2, 32, int(st.session_state.color_num), step=1, key='color_num')
+            if st.session_state.mode == 'mda':
+                st.slider('最大色数', 2, 32, int(st.session_state.color_num), step=1, key='color_num', help='最大で設定値まで色数が割り当てられる。')
+                st.slider('色閾値', 2, 32, int(st.session_state.color_thresh), step=1, key='color_thresh', help='設定値を下げるほど色数が増える。')
+            if st.session_state.mode == 'ndcd':
+                pass
+
+    # Pixelate button
+    pixelate_button = st.button('ドット化', use_container_width=True, disabled='uploaded_img' not in st.session_state)
+    if pixelate_button:
+        # Convert uploaded file to OpenCV format
+        file_bytes = np.asarray(bytearray(st.session_state.uploaded_img.read()), dtype=np.uint8)
+        cv_img = cv2.imdecode(file_bytes, 1)
+
+        # pixelate
+        st.session_state.pixelated_img = model.convert(cv_img,
+                                                       mode=st.session_state.mode,
+                                                       pixel_size=int(st.session_state.pixel_size),
+                                                       color_num=int(st.session_state.color_num),
+                                                       color_thresh=int(st.session_state.color_thresh),
+                                                       is_noise_reduction=st.session_state.is_noise_reduction)
+
+
+    image_container = st.container(border=True, height=450)
+    with image_container:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('ドット化前')
+            if 'uploaded_img' in st.session_state:
+                st.image(st.session_state.uploaded_img)
+            else:
+                st.write('画像をアップロードしてください。')
+        with col2:
+            st.write('ドット化後')
+            if 'pixelated_img' in st.session_state:
+                st.image(st.session_state.pixelated_img, channels='BGR')
+            else:
+                st.write('画像をドット化してください。')
+
+    # download button
+    buf = '' # dummy buffer
+    if 'pixelated_img' in st.session_state:
+        _, buf = cv2.imencode('.png', st.session_state.pixelated_img)
+        buf = buf.tobytes()
+    st.download_button(label='ダウンロード',
+                       data=buf,
+                       file_name='ドット化後画像.png', 
+                       mime='image/png',
+                       use_container_width=True,
+                       disabled='pixelated_img' not in st.session_state)
+
+if __name__ == '__main__':
+    initialize()
+    main()
